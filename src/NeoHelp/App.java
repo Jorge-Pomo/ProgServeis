7cc4b2963ea944a558953d5ed26e3586cbeb5f70
Jorge-Pomo <jopora@xuqueralumnat.es>
2021-10-19 10:36:35 +0200

    comentando codigo

--- src/NeoHelp/App.java
+++ src/NeoHelp/App.java
@@ -13,8 +13,10 @@ public class App {
 	// i5 - 10210U;
 	// nucleos = 4;
 	// subprocesos = 8;
-
-	// Retorna un arrayList que conte un array de 3 posicions amb les
+	
+	// leerNeo()
+	// No te cap parametres d'entrada
+	// Eixida: retorna un arrayList que conte un array de 3 posicions amb les
 	// caracteristicques dels NEOs
 	public static ArrayList<String[]> leerNeo() {
 		ArrayList<String[]> neos = new ArrayList<>();
@@ -37,28 +39,26 @@ public class App {
 
 	}
 
+	// lanzarProbabilidad(String[])
+	// Parametres d'entrada: un array de 3 posicións que conte el nom, posició i velocitat del neo.
+	// Eixida: retorna el process que s'esta ejecutant
 	public static Process lanzarProbabilidad(String[] caracteristicas) {
 
 		String n1 = caracteristicas[0];
 		String n2 = caracteristicas[1];
 		String n3 = caracteristicas[2];
 
-		// Nom de la clase a la que volem cridar
 		String clase = "NeoHelp.CalcularProbabilidadImpacto";
 
 		Process process = null;
 
 		try {
-
 			String javaHome = System.getProperty("java.home");
-			// Ruta on es troba Java instalat
 			String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
-			// Ruta de la clase que anem a cridar
 			String classpath = System.getProperty("java.class.path");
-			// System.out.println(classpath);
-			// Nom de la calse
 			String className = clase;
 
+			// Command
 			List<String> command = new ArrayList<>();
 			command.add(javaBin);
 			command.add("-cp");
@@ -69,10 +69,6 @@ public class App {
 			command.add(n1);
 			command.add(n2);
 			command.add(n3);
-
-			// System.out.println("Comando que se pasa a ProcessBuilder: " + command);
-			// System.out.println("Comando a ejecutar en cmd.exe: " +
-			// command.toString().replace(",", ""));
 
 			ProcessBuilder builder = new ProcessBuilder(command);
 			process = builder.inheritIO().start();
@@ -87,18 +83,21 @@ public class App {
 		return process;
 	}
 
+	// Main
 	public static void main(String[] args) {
+		// Iniciem temporitzador
 		long inicio = System.currentTimeMillis();
 
 		Process process = null;
 
 		ArrayList<String[]> contenido = leerNeo();
 
+		// funció que retorna el num de subprocesos que pot ejecutar el processador
 		int numCores = Runtime.getRuntime().availableProcessors();
 		int cont = 0;
 
+		// calculem sols la probabilitat de 8 NEOs
 		for (int i = 0; i < numCores; i++) {
-
 			process = lanzarProbabilidad(contenido.get(cont));
 			cont++;
 		}
@@ -106,13 +105,8 @@ public class App {
 		try {
 			process.waitFor();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		System.out.println("8");
-
-		System.out.println("Fin");
 
 		// Calcular temps
 		long fin = System.currentTimeMillis();
@@ -121,8 +115,8 @@ public class App {
 
 		try {
 			Thread.sleep(500);
+			System.out.println("Fin");	
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
--- src/NeoHelp/CalcularProbabilidadImpacto.java
+++ src/NeoHelp/CalcularProbabilidadImpacto.java
@@ -7,12 +7,13 @@ import java.text.DecimalFormat;
 
 public class CalcularProbabilidadImpacto {
 
+	//Main
 	public static void main(String[] args) {
 		String nombreNEO = args[0];
 		double posicionNEO = Double.parseDouble(args[1]);
 		double velocidadNEO = Double.parseDouble(args[2]);
 
-		// Calculo
+		// Calculo de probabilidad
 		double posicionTierra = 1;
 		double velocidadTierra = 100;
 		for (int i = 0; i < (50 * 365 * 24 * 60 * 60); i++) {
@@ -22,19 +23,27 @@ public class CalcularProbabilidadImpacto {
 		double resultado = 100 * Math.random() *
 		Math.pow( ((posicionNEO-posicionTierra)/(posicionNEO+posicionTierra)), 2);
 		
-		// Escribir en txt
-		guardarProbabilidad(resultado, nombreNEO);
+		// Enviem parametres a guardarProbabilidad
+		if(guardarProbabilidad(resultado, nombreNEO)) {
+			System.out.println("Exit en l'escriptura");
+		}else {
+			System.err.println("Error, l'escriptura ha fallat");
+		}
 		
+		// Li donem format al double
 		double formatResu = (double)Math.round(resultado * 100d) / 100d;
 		
+		// Mensaje por terminal
 		if(formatResu >= 10) {
-			System.err.println("¡Alerta mundial! Alta probabilidad de impacto por un asteroide!!!");
+			System.err.println("¡Alerta mundial! Hay un " + formatResu + " probabilidad de impacto por un asteroide!!!");
 		}else {
 			System.out.println("Solo hay " + formatResu + "% de probabilidad de un impacto, no hay que preocuparse");
 		}
 	}
 	
-	// Guardar resultat
+	// guardarProbabilidad(double, String)
+	// Parametres d'entrada: un double amb la probabilitat d'impacte y un String amb el nom del NEO
+	// Eixida: un boolean diguent si l'escritura a tingut exit o no.
 	private static boolean guardarProbabilidad(double resuProbabilidad, String nombre) {
 		boolean resu = false;
 		
@@ -42,6 +51,7 @@ public class CalcularProbabilidadImpacto {
 			BufferedWriter bw = new BufferedWriter(new FileWriter(".\\src\\NeoHelp\\Arxius\\resultadoProbabilidad\\" + nombre));
 			bw.write("" + resuProbabilidad);
 		
+			resu = true;
 			bw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
